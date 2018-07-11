16892b6258e584047a7d7fc23eee0c40996d9a81
Ochoa <krvr618@astrazeneca.net>
2018-07-11 11:40:22 -0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -62,7 +62,6 @@ app.delete('/todos/:id', (req, res) => {
     }
     res.status(200).send({todo});
   }).catch((e) => res.status(400).send());
-
 });
 
 app.patch('/todos/:id', (req, res) => {
@@ -89,7 +88,19 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
