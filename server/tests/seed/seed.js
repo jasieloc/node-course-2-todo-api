6146f78720467ad5e023bf2fa1a1e92a6fb4f41e
Ochoa <krvr618@astrazeneca.net>
2018-07-16 12:14:44 -0500

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -4,7 +4,7 @@ const {Todo} = require('./../../models/todo');
 const {User} = require('./../../models/user');
 
 const userOneId = new ObjectID();
-const UserTwoId = new ObjectID();
+const userTwoId = new ObjectID();
 
 const users = [{
     _id: userOneId,
@@ -15,19 +15,25 @@ const users = [{
       token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123!').toString()
     }]
 }, {
-    _id: UserTwoId,
+    _id: userTwoId,
     email: 'di@example.com',
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [{
+      access: 'auth',
+      token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123!').toString()
+    }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo 4'
+  text: 'First test todo 4',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
